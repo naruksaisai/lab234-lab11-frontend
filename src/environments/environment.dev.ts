181214b106d2f75fbba1033a3b7e7b8d3a43848a
Unknown <joniejostar@gmail.com>
2018-04-25 14:55:58 +0700

    0.3#3 username...

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  graphqlUri: 'http://54.169.49.224:8084/graphql'
+  graphqlUri: 'http://54.169.49.224:8083/graphql'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  graphqlUri: 'http://54.255.174.42:8084/graphql'
+  graphqlUri: 'http://54.255.174.42:8083/graphql'
 
 };
